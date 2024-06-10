f91425093284bf7c4e3abc1a2a08fa0ae33d0aec
Seongyeong Ju <jus@oregonstate.edu>
2024-06-10 14:05:01 -0700

    CRUD with new table implemented

--- frontend/src/components/Employees.jsx
+++ frontend/src/components/Employees.jsx
@@ -1,10 +1,12 @@
 import JsonDataDisplay from './table';
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
-import Dropdown from './dropdown';
-
-
-
+import EditableTable from './EditableTable';
+import EditableCell from './EditableCell';
+import { DeleteIcon } from '@chakra-ui/icons'
+import { createNewTableContext } from './TableDataContext';
+  
+  
 export default function Employee() {
   const [update_employee_id, set_update_employee_id] = useState('');
   const [update_employee_new_name, set_update_employee_new_name] = useState('');
@@ -15,124 +17,93 @@ export default function Employee() {
   const [create_employee_new_role, set_create_employee_new_role] = useState('');
   const [delete_employee_id, set_delete_employee_id] = useState('');
   const [employee_data, set_employee_data] = useState([]);
-  const [message, setMessage] = useState('');
+  const [initialData, setInitialData] = useState([]);
 
-  const fetchEmployees = async () => {
-    try {
-      const URL = import.meta.env.VITE_API_URL + "Employees";
-      const response = await axios.get(URL, {
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
-      console.log(response.data);
-      set_employee_data(response.data);
-    } catch (error) {
-      console.error('Error:', error);
-      setMessage('An error occurred while getting the employees.');
-    }
-  };
-
+  // Fetch data from API when component mounts
   useEffect(() => {
-    fetchEmployees();
-  }, []);
-
-  const handleUpdateEmployee = async () => {
-    const data = {
-      id: update_employee_id, // Ensure backend expects this format
-      name: update_employee_new_name, // Ensure backend expects this format
-      role: update_employee_new_role,
-    };
-    try {
-      const URL = import.meta.env.VITE_API_URL + "Employees";
-      const response = await axios.put(URL, data, {
-        headers: {
-          'Content-Type': 'application/json'
-        },
-      });
-
-      if (response.data.error) {
-        setMessage(response.data.error)
+    const fetchData = async () => {
+      try {
+        const URL = import.meta.env.VITE_API_URL + "Employees";
+        const response = await axios.get(URL, {
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+        setInitalData(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
       }
-      fetchEmployees();
-    } catch (error) {
-      console.error('Error:', error);
-      setMessage('An error occurred while updating the employee.');
-    }
-  };
-
-  const handleCreateEmployee = async () => {
-    const data = {
-      name: create_employee_new_name,
-      role: create_employee_new_role,
-      department: selectedDepartment,
     };
+    fetchData(); // Call the fetchData function
+  }, []); // Empty dependency array ensures useEffect runs only once on mount
+
+
+  const handleDeleteRow = async (employeeID) => {
     try {
       const URL = import.meta.env.VITE_API_URL + "Employees";
-      await axios.post(URL, data, {
-        headers: {
-          'Content-Type': 'application/json'
-        },
-      });
-
-      setMessage('New employee created successfully!');
-      fetchEmployees();
-    } catch (error) {
-      console.error('Error:', error);
-      setMessage('An error occurred while creating the new employee.');
-    }
-  };
-
-  const handleDeleteEmployee = async () => {
-    try {
-      const URL = import.meta.env.VITE_API_URL + "Employees";
-      await axios.delete(URL, {
+      const response = await axios.delete(URL, {
         headers: {
           'Content-Type': 'application/json'
         },
         data: {
-          id: delete_employee_id
-        },
+          employee_id: employeeID
+        }
       });
+      console.log('Row deleted:', response.data);
 
-      setMessage('Employee deleted successfully!');
-      fetchEmployees();
+      // Update the table data (remove the deleted row)
+      const updatedData = initialData.filter((row) => row.employee_id !== employeeID);
+      setInitialData(updatedData);
     } catch (error) {
-      console.error('Error:', error);
-      setMessage('An error occurred while deleting the employee.');
+      console.error('API request error:', error);
+      // Handle error (e.g., show error message to the user)
     }
   };
 
-  const handleGetEmployees = async () => {
-    fetchEmployees();
-  };
 
-  const handleGetDepartmentNames = async () => {
-    try {
-      const URL = import.meta.env.VITE_API_URL + 'Departments/Names';
-      const response = await axios.get(URL, {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-      // Transform the data
-      const transformedData = response.data.map(department => ({
-        value: department.department_id,
-        label: department.name
-      }));
-      setDepartmentNames(transformedData);
-    } catch (error) {
-      console.error('Error:', error);
-      setMessage('An error occurred while fetching the department names.');
+  const columns = [
+    {
+      accessorKey: 'employee_id',
+      header: 'Employee ID',
+      cell: (props) => <p>{props.getValue()}</p>,
+      editable: false,
+      isNew: false
+    },
+    {
+      accessorKey: 'name',
+      header: 'Employee Name',
+      cell: (props) => <EditableCell {...props} endpoint="Employees" />,
+      editable: true,
+      isNew: false
+    },
+    {
+      accessorKey: 'role',
+      header: 'Role',
+      cell: (props) => <EditableCell {...props} endpoint="Employees" />,
+      editable: true,
+      isNew: false
+    },
+    {
+      accessorKey: 'department',
+      header: 'Department',
+      cell: (props) => <EditableCell {...props} endpoint="Employees" />,
+      editable: true,
+      isNew: false
+    },
+    {
+      accessorKey: 'delete_row',
+      header: '',
+      cell: (props) => (
+        <DeleteIcon
+          onClick={() => handleDeleteRow(props.row.original.employees_id)} // Call handleDeleteRow function
+          cursor="pointer"
+          color="red.500"
+        />
+      ),
+      editable: false,
+      isNew: false
     }
-  };
-  useEffect(() => {
-    handleGetDepartmentNames();
-  }, []);
-
-  const handleSelectedChange = (selectedValue) => {
-    setSelectedDepartment(selectedValue)
-  };
+  ]
 
   return (
     <div>
@@ -140,67 +111,11 @@ export default function Employee() {
         <h1>Employee Management</h1>
       </header>
       <div className="App-body">
-        <div className="right-half">
-          <h2>Employee Table</h2>
-          <JsonDataDisplay JSONdata={employee_data} endpoint={"Employees"} />
-        </div>
-
-        <div className="left-half">
-          <h2>Update Employees</h2>
-          <input
-            type="number"
-            placeholder="Employee ID"
-            value={update_employee_id}
-            onChange={(e) => set_update_employee_id(e.target.value)}
-          />
-          <input
-            type="text"
-            placeholder="New Employee Name"
-            value={update_employee_new_name}
-            onChange={(e) => set_update_employee_new_name(e.target.value)}
-          />
-          <input
-            type="text"
-            placeholder="New Employee Role"
-            value={update_employee_new_role}
-            onChange={(e) => set_update_employee_new_role(e.target.value)}
-          />
-          <button onClick={handleUpdateEmployee}>Update Employee</button>
-          {message && <p>{message}</p>}
-
-          <h2>Create Employee</h2>
-          <input
-            type="text"
-            placeholder="Employee Name"
-            value={create_employee_new_name}
-            onChange={(e) => set_create_employee_new_name(e.target.value)}
-          />
-          <input
-            type="text"
-            placeholder="Employee Role"
-            value={create_employee_new_role}
-            onChange={(e) => set_create_employee_new_role(e.target.value)}
-          />
-          <Dropdown optionArr={departmentNames} onSelectChange={handleSelectedChange} itemLabel={"Department ID: "} />
-          <button onClick={handleCreateEmployee}>Create New Employee</button>
-          {message && <p>{message}</p>}
-
-          <h2>Get Employees</h2>
-          <button onClick={handleGetEmployees}>Get Employees</button>
-          {message && <p>{message}</p>}
-
-          <h2>Delete Employee</h2>
-          <input
-            type="number"
-            placeholder="Employee ID"
-            value={delete_employee_id}
-            onChange={(e) => set_delete_employee_id(e.target.value)}
-          />
-          <button onClick={handleDeleteEmployee}>Delete Employee</button>
-          {message && <p>{message}</p>}
-        </div>
+        <TableDataProvider initialData={initialData}>
+          <EditableTable endpoint={'Employees'} columns={columns} />
+        </TableDataProvider>
       </div >
-    </div >
+    </div>
 
   );
 }
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+
 const Home = () => {
     return (
-        <div className="home">
-            <h2>In Home</h2>
-            <p>To see changes in displayed tables you must refresh the page sorry :/</p>
+        <div>
+        <h1>MedCare Clinic Patient Scheduler</h1>
+        <h2>Home Page for Team 39</h2>
+        <p>Authors: Seongyeong Ju and Micah Jazen</p>
+        <article>
+            <p><strong>Employees</strong>: Displays list of all employees, click here to add or remove employees</p>
+            <p><strong>Employee-Schedule</strong>: Displays list of employee ID's to the corresponding schedule slot ID</p>
+            <p><strong>Patients</strong>: Displays list of all patients, click here to add or remove patients</p>
+            <p><strong>Patient-Schedule</strong>: Displays list of patient ID's to the corresponding schedule slot ID</p>
+            <p><strong>Departments</strong>: Click here to view all departments in the clinic</p>
+            <p><strong>Procedures</strong>: Click here to view all available procedures</p>
+            <p><strong>Schedules</strong>: Displays complete schedule of clinic, click here to create or cancel a scheduled appointment</p>
+        </article>
+        
         </div>
     );
 };
