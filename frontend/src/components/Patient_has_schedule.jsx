dadd297d9820fb66cb9fe1f1bbf9aac1cf68da33
Sarah Ju <jus@oregonstate.edu>
2024-06-10 21:32:53 -0700

    Update Patient_has_schedule.jsx

--- frontend/src/components/Patient_has_schedule.jsx
+++ frontend/src/components/Patient_has_schedule.jsx
@@ -44,7 +44,7 @@ export default function Patient_has_schedule() {
             set_patient_has_schedule_data(response.data);
         } catch (error) {
             console.error('Error:', error);
-            setMessage('An error occurred while fetching the department names.');
+            setMessage('An error occurred while fetching the Patient_has_schedule data.');
         }
     };
     useEffect(() => {
@@ -54,20 +54,14 @@ export default function Patient_has_schedule() {
     return (
         <div>
             <header className="App-header">
-                <h1>patient_has_schedule Management</h1>
+                <h1>Patient-Schedule Management</h1>
             </header>
             <div className="App-body">
                 <div className="right-half">
-                    <h2>patient_has_schedule Table</h2>
+                    <h2>Patient-Schedule Table</h2>
                     <JsonDataDisplay JSONdata={patient_has_schedule_data} endpoint={"Patients_has_Schedule"} />
                 </div>
 
-                <div className="left-half">
-                    <h2>Get patient_has_schedules</h2>
-                    <button onClick={handleGetPatientHasSchedules}>Get patient_has_schedules</button>
-                    {message && <p>{message}</p>}
-
-                </div>
             </div >
         </div >
 
