6f43123c03ce78755da2d45e470b86ec7e3c789d
Sarah Ju <jus@oregonstate.edu>
2024-06-10 21:30:52 -0700

    Update Home.jsx

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,19 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Department from './Departments';
+import Employee from './Employees';
+import Patient from './Patients';
+import Procedure from './Procedure';
+import Schedule from './Schedules';
+import Employee_has_schedule from './Employee_has_schedule';
+import Patient_has_schedule from './Patient_has_schedule';
 
 const Home = () => {
     return (
         <div>
-        <h1>MedCare Clinic Patient Scheduler</h1>
+        <br />
+        <h1><strong>MedCare Clinic Patient Scheduler</strong></h1>
         <h2>Home Page for Team 39</h2>
         <p>Authors: Seongyeong Ju and Micah Jazen</p>
+        <br />
         <article>
-            <p><strong>Employees</strong>: Displays list of all employees, click here to add or remove employees</p>
-            <p><strong>Employee-Schedule</strong>: Displays list of employee ID's to the corresponding schedule slot ID</p>
-            <p><strong>Patients</strong>: Displays list of all patients, click here to add or remove patients</p>
-            <p><strong>Patient-Schedule</strong>: Displays list of patient ID's to the corresponding schedule slot ID</p>
-            <p><strong>Departments</strong>: Click here to view all departments in the clinic</p>
-            <p><strong>Procedures</strong>: Click here to view all available procedures</p>
-            <p><strong>Schedules</strong>: Displays complete schedule of clinic, click here to create or cancel a scheduled appointment</p>
+            <p><u><Link to="/Employees">Employees</Link></u>: Displays list of all employees, click here to add or remove employees</p>
+            <p><u><Link to="/Employee_has_schedule">Employee-Schedule</Link></u>: Displays list of employee ID's to the corresponding schedule slot ID</p>
+            <p><u><Link to="/Patients">Patients</Link></u>: Displays list of all patients, click here to add or remove patients</p>
+            <p><u><Link to="/Patient_has_schedule">Patient-Schedule</Link></u>: Displays list of patient ID's to the corresponding schedule slot ID</p>
+            <p><u><Link to="/Departments">Departments</Link></u>: Click here to view all departments in the clinic</p>
+            <p><u><Link to="/Procedures">Procedures</Link></u>: Click here to view all available procedures</p>
+            <p><u><Link to="/Schedules">Scheduless</Link></u>: Displays complete schedule of clinic, click here to create or cancel a scheduled appointment</p>
         </article>
         
         </div>
